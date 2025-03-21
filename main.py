888364e39b51fc175423251fcab250514418b3fd
bhushan raut <braut0069@gmail.com>
2025-03-22 03:45:48 +0530

    Fixed spam detection issues

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, jsonify
 import joblib
 import os
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Allow frontend to access API
 
 app = Flask(__name__)
 
